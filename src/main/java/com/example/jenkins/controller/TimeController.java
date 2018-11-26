4eac5a56eebf0d5958c22c46bb85e5ea690874ab
hy_liangzd <517610366@qq.com>
2018-11-26 21:13:25 +0800

    commit new file

---
+++ src/main/java/com/example/jenkins/controller/TimeController.java
@@ -0,0 +1,20 @@
+package com.example.jenkins.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/time")
+public class TimeController {
+	
+	@RequestMapping(value="/now")
+	public String now(@PathVariable int size) {
+		SimpleDateFormat simple = new SimpleDateFormat("HH:mm:ss");
+		System.out.println(simple.format(new Date()));
+		return "now is :"+simple.format(new Date());
+	}
+}
